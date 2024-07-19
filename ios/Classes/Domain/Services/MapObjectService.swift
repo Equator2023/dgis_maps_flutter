371d840ec9d0373abd2a7d88f1669e9036bae075
999 <iamthegreatthanos@gmail.com>
2024-07-20 01:11:46 +0500

    Added some changes.

--- ios/Classes/Domain/Services/MapObjectService.swift
+++ ios/Classes/Domain/Services/MapObjectService.swift
@@ -116,7 +116,7 @@ final class MapObjectService {
          for markerData in markerUpdates.toAdd {
              if let data = markerData {
                  let newMarker = data2Marker(data: data)
-                 markers[data.markerId.value] = newMarker!
+                 markers[data.markerId.value!] = newMarker
                  if let marker = newMarker {
                      mapObjectManager.addObject(item: marker)
                  }
